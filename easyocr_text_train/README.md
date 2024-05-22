1e2ec5df1553901c114c446f5003485cdc6d1f2f
Pattanan Aphiwetsa <monz@Pattanans-MacBook-Pro.local>
2024-05-22 16:28:36 -0700

    training file

---
+++ easyocr_text_train/README.md
@@ -0,0 +1,11 @@
+# Training EasyOCR Moel
+
+## Resources
+
+https://github.com/clovaai/deep-text-recognition-benchmark
+
+## Procedure
+- download desired fonts
+- generate dataset by Pillow library
+- organize the data folder and then obtain mdb file from the fire library
+- download the pre-train model
--- easyocr_text_train/generate_synthetic_images.py
+++ easyocr_text_train/generate_synthetic_images.py
@@ -20,27 +20,41 @@ def get_text_from_tha_file():
 # generate with pillow image
 
 
-def generate_images_with_pillow(file_path, output_folder_path, font_path):
+def generate_images_with_pillow(file_path, output_folder_path, font_path, text_file_path):
     with open(file_path, "r") as read_file:
         lines = read_file.readlines()
         index = 0
+        # shuffle the line
         for line in lines:
             image_width, image_height = 1000, 100
             image = Image.new('RGB', (image_width, image_height), color="white")
             draw = ImageDraw.Draw(image) 
             font = ImageFont.truetype(font_path, 30)
             draw.text((30, 30), line, font=font, fill=(0,0,0))
-            image.save(os.path.join(output_folder_path, str(index) + ".png"))
+            if (line.rstrip('\n') == '.' or line.rstrip('\n') == '/' ):
+                print(True)
+                continue
+            image.save(os.path.join(output_folder_path, line.rstrip('\n') + ".png"))
+            write_path_on_text_file(text_file_path, os.path.join(output_folder_path, line.rstrip('\n') + ".png"), line.rstrip('\n'))
             index += 1
             if (index > 1000):
                 break
 
 
+def write_path_on_text_file(text_file_path, file_path, label):
+    with open(text_file_path, 'a') as text_file:
+        text_file.write(file_path + '\t' + label + '\n')
+
+
+
 def main():
     input_path = "datasets/tha_dataset.txt"
     output_folder_path = "pillow_images_generated"
-    font_path = "fonts/Pattaya/Pattaya-Regular.ttf"
-    generate_images_with_pillow(input_path, output_folder_path, font_path)
+    pattaya_font_path = "fonts/Pattaya/Pattaya-Regular.ttf"
+    ayutthaya_font_path = "fonts/Ayutthaya/Ayutthaya/Ayutthaya.ttf"
+    text_file_path = "gt.txt"
+    # generate_images_with_pillow(input_path, output_folder_path, pattaya_font_path)
+    generate_images_with_pillow(input_path, output_folder_path, pattaya_font_path, text_file_path)
 
 
 if __name__ == "__main__":
